8830d7b60da15bb41e47d9432fb9bb3c9e46483d
Shashank171204 <shashu1712@gmail.com>
2025-04-23 09:25:52 +0530

    update

--- q20.js
+++ q20.js
@@ -1,12 +1,21 @@
-db.employee.find({
+db.employees.find({
     $or: [
-        {salary:{$gt:2000}},
-        {department:{$eq:"CSE"}}
-    ]
-});
-db.employee.find({
-    $and: [
-        {salary:{$gt:2000}},
-        {department:{$eq:"CSE"}}
-    ]
-});
+      { salary: { $gt: 2000 } }, 
+      { department: { $eq: "HR" } }
+  ],
+  });
+  
+  db.employees.find({
+      $and: [
+        { salary: { $gt: 2000 } }, 
+        { department: { $eq: "HR" } }
+    ],
+    });
+  
+  
+    db.employees.find({
+      $nor: [
+        { salary: { $gt: 2000 } }, 
+        { department: { $eq: "HR" } }
+    ],
+    });
